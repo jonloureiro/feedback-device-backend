dbf782936c3d8b088c2ac8daec00e54aa5e4cc24
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2019-07-14 08:51:47 -0400

    colocando middlewares error na pasta correta

--- src/index.js
+++ src/index.js
@@ -2,22 +2,13 @@ const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 
 const { port } = require('./config');
+const { error } = require('./middlewares');
 
 
 const app = new Koa();
 
 app
-  .use(async (ctx, next) => {
-    try {
-      await next();
-    } catch (err) {
-      ctx.status = err.status || 500;
-      ctx.body = {
-        message: err.message,
-      };
-      if (ctx.status === 500) ctx.app.emit('error', err, ctx);
-    }
-  })
+  .use(error)
   .use(bodyParser())
   .listen(port);
 
