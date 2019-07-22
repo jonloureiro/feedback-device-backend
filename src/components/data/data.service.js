590d66dedaa24b2b338641af02361620c0179389
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2019-07-22 15:05:27 -0400

    criando o get para /data

--- src/components/data/data.service.js
+++ src/components/data/data.service.js
@@ -20,7 +20,56 @@ const save = async ({ value, createAt }, mcu) => {
 
 const take = async (user) => {
   if (!await checkUser(user)) throw error(401, 'Usuário não autorizado');
-  return { message: 'EM CONSTRUÇÃO' };
+  try {
+    const date = new Date();
+    // getMonth está devolvendo -1, logo janeiro está com 0;
+    const year = (date.getMonth() === 0) ? date.getFullYear() - 1 : date.getFullYear();
+    const month = (date.getMonth() === 0) ? 12 : date.getMonth();
+    const today = date.getDate();
+
+    const data = await Data.find({
+      createAt: {
+        $gte: new Date(`${year},${month},${today}`).toISOString(),
+      },
+    });
+
+    const response = data.reduce((acc, cur) => {
+      const objDate = new Date(cur.createAt);
+      // const objMonth = objDate.getMonth();
+      const objDay = objDate.getDate();
+      let attr = '';
+
+      // COMPARAR PRIMEIRO O MÊS!
+
+      if (objDay === today) attr = 'today';
+      else if (objDay > today - 7) attr = 'week';
+      else attr = 'month';
+
+      switch (cur.value) {
+        case 0:
+          acc[attr][0] += 1;
+          break;
+        case 1:
+          acc[attr][1] += 1;
+          break;
+        case 2:
+          acc[attr][2] += 1;
+          break;
+        default:
+          break;
+      }
+
+      return acc;
+    }, {
+      today: [0, 0, 0],
+      week: [0, 0, 0],
+      month: [0, 0, 0],
+    });
+
+    return response;
+  } catch (err) {
+    throw error(400, 'Erro ao consultar dados');
+  }
 };
 
 
